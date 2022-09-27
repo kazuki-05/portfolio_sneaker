902182a9addb58f6e5ab37e4eeadeb25ff00c721
kazuki-05 <ec2-user@ip-172-31-9-243.ap-northeast-1.compute.internal>
2022-09-27 09:24:06 +0000

    finish

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,9 +49,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+  if @post.update(post_params)
+    redirect_to post_path(@post.id)
+  else
+    render 'edit'
+
+  end
   end
 
 
