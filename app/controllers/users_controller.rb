f2226bacdcb49a08b9ac4de19de32fb7c0c82f9d
kazuki-05 <ec2-user@ip-172-31-9-243.ap-northeast-1.compute.internal>
2022-10-09 08:30:12 +0000

    nicknameadd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:nickname, :profile_image)
   end
 
 end
